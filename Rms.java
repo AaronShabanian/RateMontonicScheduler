d3cb4cd2126f29440d9bf3036ef2fa721f6ff229
unknown <Shabanian@chapman.edu>
2021-04-28 16:00:16 -0700

    Setup of 2d array

--- Rms.java
+++ Rms.java
@@ -1,8 +1,19 @@
+import java.util.Arrays;
 public class Rms extends Thread {
     public Rms()
     {
 
     }
     public static void main(String[] args){
+        doWork();
+    }
+    public static void doWork(){
+        int[][] arr = new int[10][10];
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                arr[i][j]=1;
+            }
+        }
+        System.out.println(arr[3][4]);
     }
 }
